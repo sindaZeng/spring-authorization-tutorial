86b7da8de5a51f5f7617c842ca4e82f611d4fc49
Sinda <1692488900@qq.com>
2022-07-08 22:50:21 +0800

    :tada: 007

--- client2/src/main/java/com/xhuicloud/authorization/client/config/SecurityConfig.java
+++ client2/src/main/java/com/xhuicloud/authorization/client/config/SecurityConfig.java
@@ -1,9 +1,20 @@
 package com.xhuicloud.authorization.client.config;
 
+import com.xhuicloud.authorization.client.xhuicloud.XhuiCloudOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DelegatingOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -12,11 +23,16 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService =
+                new XhuiCloudOAuth2UserService();
         http
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests.anyRequest().authenticated()
                 )
+                .oauth2Login(loginConfigurer -> loginConfigurer.userInfoEndpoint().userService(oAuth2UserService))
                 .oauth2Client(withDefaults());
         return http.build();
     }
+
 }
